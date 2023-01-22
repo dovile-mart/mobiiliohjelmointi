3e79d9f2f4aad00c3b4e208468f15613ce38d735
Dovile Mart <doviliu@gmail.com>
2023-01-22 23:19:00 +0200

    Lisätty toinen rivi

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Expo appin testaus</Text>
+      <Text>Toinen rivi</Text>
       <StatusBar style="auto" />
     </View>
   );
